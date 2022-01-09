3bec7f7ae9dbccc075b1f83df37a75470a250579
Miki Yutani <mkyutani@gmail.com>
2022-01-09 14:26:03 +0900

    Multiple locations in server

--- setup.sh
+++ setup.sh
@@ -4,17 +4,33 @@
 cp -f nginx.conf.template env/nginx.conf
 cp -f default.conf.template env/default.conf
 
-# Generate rp.conf
-echo -n > env/rp.conf
-for s in `grep -v ^# env/servers.csv | sed -r '/^[^,]+,\/,/!d'`
+# Separate servers.csv into each server's
+SERVERS=`grep -v ^# env/servers.csv | cut -d, -f1 | sort | uniq`
+
+# Generate server definition
+for s in $SERVERS
 do
-	eval `echo $s | sed -r 's/^([^,]+),([^,]+),([^,]+),([^,]+)(.*)/SERVER=\1 LOCATION=\2 CONTAINER=\3 CONTAINER_PORT=\4 envsubst \\\\\\\"\\\\\\\$SERVER \\\\\\\$LOCATION \\\\\\\$CONTAINER \\\\\\\$CONTAINER_PORT\\\\\\\" < \.\/rp\.conf\.template >>env\/rp\.conf/'`
+    sed "/^$s/!d" env/servers.csv > env/servers.$s.csv.tmp
+    if [ `egrep -c -v '^[^,]+,/,' env/servers.$s.csv.tmp` -ne 0 ]
+    then
+        echo -n > env/rp.$s.conf.tmp
+        SERVER=$s envsubst "\$SERVER" < ./rp-noroot.conf.header.template >> env/rp.$s.conf.tmp
+        for l in `cat env/servers.$s.csv.tmp`
+        do
+	    eval `echo $l | sed -r 's/^([^,]+),([^,]+),([^,]+),([^,]+)(.*)/SERVER=\1 LOCATION=\2 CONTAINER=\3 CONTAINER_PORT=\4 envsubst \\\\\\\"\\\\\\\$SERVER \\\\\\\$LOCATION \\\\\\\$CONTAINER \\\\\\\$CONTAINER_PORT\\\\\\\" < \.\/rp-noroot\.conf\.location\.template >> env\/rp\.\1.conf.tmp/'`
+        done
+        SERVER=$s envsubst "\$SERVER" < ./rp-noroot.conf.trailer.template >> env/rp.$s.conf.tmp
+    else
+        echo -n > env/rp.$s.conf.tmp
+        for l in `cat env/servers.$s.csv.tmp`
+        do
+            eval `echo $l | sed -r 's/^([^,]+),([^,]+),([^,]+),([^,]+)(.*)/SERVER=\1 LOCATION=\2 CONTAINER=\3 CONTAINER_PORT=\4 envsubst \\\\\\\"\\\\\\\$SERVER \\\\\\\$LOCATION \\\\\\\$CONTAINER \\\\\\\$CONTAINER_PORT\\\\\\\" < \.\/rp\.conf\.template >> env\/rp\.\1.conf.tmp/'`
+        done
+    fi
 done
 
-for s in `grep -v ^# env/servers.csv | sed -r '/^[^,]+,\/,/d'`
-do
-	eval `echo $s | sed -r 's/^([^,]+),([^,]+),([^,]+),([^,]+)(.*)/SERVER=\1 LOCATION=\2 CONTAINER=\3 CONTAINER_PORT=\4 envsubst \\\\\\\"\\\\\\\$SERVER \\\\\\\$LOCATION \\\\\\\$CONTAINER \\\\\\\$CONTAINER_PORT\\\\\\\" < \.\/rp-noroot\.conf\.template >>env\/rp\.conf/'`
-done
+# Concatenate all server definitions
+cat env/*.conf.tmp > env/rp.conf
 
 # Generate docker-compose.yml
 echo -n > env/docker-compose.yml
@@ -29,3 +45,6 @@ for s in `grep -v ^# env/servers.csv | cut -d, -f5 | sort | uniq`
 do
         CONTAINER_NETWORK=$s envsubst "\$CONTAINER_NETWORK" < ./docker-compose.yml.networks.placeholder.template >> env/docker-compose.yml
 done
+
+# Clean up temporary files
+rm env/*.tmp
